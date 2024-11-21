611a3193681ef0dd2bdbf2cc3b88073dcf1d0349
Farzin Kazemzadeh <itisFarzin@gmail.com>
2024-11-21 10:14:07 +0330

    Fix build script
    - Add missing environment variable exports
    - Change setup script to rsuntk's KernelSU
    - Change back to previous directory after packing AnyKernel
    
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- build.sh
+++ build.sh
@@ -6,8 +6,13 @@ git clone https://github.com/itisFarzin/android_prebuilts_clang_host_linux-x86_c
 export PATH="$(pwd)/clang/bin:$PATH"
 export LD_LIBRARY_PATH="$(pwd)/clang/lib64:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$(pwd)/clang/lib:$LD_LIBRARY_PATH"
+export ARCH=arm64
+export SUBARCH=ARM64
+export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-gnu-
-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+export KBUILD_BUILD_USER="itisFarzin"
+export KBUILD_BUILD_HOST="itisFarzin"
 today=$(date +"%Y%m%d")
 
 androidversion=$1
@@ -28,7 +33,7 @@ if [ -d "drivers/kernelsu" ]; then
   rm -rf "drivers/kernelsu"
 fi
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -
 
 make O=out CC=clang k6877v1_64_k419_defconfig LLVM=1 LLVM_IAS=1 LD=ld.lld HOSTLD=ld.lld HOSTLDFLAGS=-fuse-ld=lld
 make O=out CC=clang -j$(nproc --all) HOSTCC=$(pwd)/clang/bin/clang HOSTCXX=$(pwd)/clang/bin/clang++ LD=$(pwd)/clang/bin/ld.lld AR=$(pwd)/clang/bin/llvm-ar LLVM=1 LLVM_IAS=1 CC=" clang --cuda-path=/dev/null"
